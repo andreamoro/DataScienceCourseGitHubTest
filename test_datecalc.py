962390ca6640e6804ab8e4c8f1f0bbc368290070
Andrea Moro <andrea.moro@rs-components.com>
2021-10-21 18:24:16 +0200

    Changes to the test

--- test_datecalc.py
+++ test_datecalc.py
@@ -15,15 +15,11 @@ def tomorrow(today) -> datetime.date:
     return today + timedelta(days=1)
 
 def test_duration(today, yesterday):
-    # This test aims to verify whether the calculator refuse an 
-    # ending date past the start date.
-    #
-    # It is expected to fail
-    assert duration(today, yesterday) < 0
+    # This test aims to verify whether the day calculation
+    # returns a positive number 
+    assert duration(today, yesterday) >= 0
 
 def test_futuredate(tomorrow):
     # This test aims to verify whether the returned date is 
     # greater than the input date.
-    #
-    # It is expect to succeed
     assert when(tomorrow, 5) > tomorrow
