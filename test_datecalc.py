9c9cb1870791039af58f02604ff73fe7d318a6fc
Andrea Moro <andrea.moro@rs-components.com>
2021-10-22 18:58:25 +0200

    Changing test

--- test_datecalc.py
+++ test_datecalc.py
@@ -1,19 +1,6 @@
-import pdb
 import pytest
 from datecalc import *
 from datetime import date as dt, timedelta
-
-# @pytest.fixture
-# def today() -> datetime.date:
-#     return dt.today()
-
-# @pytest.fixture
-# def yesterday(today) -> datetime.date:
-#     return today + timedelta(days=-1)
-
-# @pytest.fixture
-# def tomorrow(today) -> datetime.date:
-#     return today + timedelta(days=1)
 
 def today() -> datetime.date:
     return dt.today()
@@ -28,18 +15,6 @@ def tomorrow() -> datetime.date:
 def raise_error():
     # pytest.fail(f"ERROR: {msg}")
     raise Exception(f"ERROR:")
-
-# @pytest.mark.parametrize("start_date, end_date", [[today(), yesterday()], [yesterday(), today()]])
-# def test_duration(start_date, end_date):
-#     # This test aims to verify whether the day calculation
-#     # returns a positive number 
-#     # Using the pytest decorator I can run multiple tests at once.
-#     assert duration(start_date, end_date) >= 0
-
-# def test_futuredate(tomorrow):
-#     # This test aims to verify whether the returned date is 
-#     # greater than the input date.
-#     assert when(tomorrow, 5) > tomorrow
 
 def test_input_is_correct_format():
     # This test aims intentionally try to test for a string 
